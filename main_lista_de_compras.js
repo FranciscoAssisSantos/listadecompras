42795ce875492f8f17891553d8a884744a553539
FranciscoAssisSantos <97245629+FranciscoAssisSantos@users.noreply.github.com>
2022-07-02 08:40:54 -0400

    Update main_lista_de_compras.js

--- main_lista_de_compras.js
+++ main_lista_de_compras.js
@@ -5,13 +5,20 @@ function somar(){
     var soma2 = document.querySelector("#n2");
     var num1 = parseFloat(soma1.value);
     var num2 = parseFloat(soma2.value);
+     
 
-
-
+    
+    for (passo = 0; passo++) {
+    // Executa 5 vezes, com os valores de passos de 0 a 4.
+    write(num1):
+     }
+    
+    
+    /*
     var total = (num1) + (num2);
     const v_total = total.toLocaleString('pt-BR', {style:'currency', currency:'BRL'});
     document.querySelector("#res").innerHTML=v_total;
     document.querySelector("#h1-total").innerHTML="TOTAL: " + v_total;
 
-}
+}*/
 // ⇓⇓⇓⇓ SCRIPT EM FASE DE TESTE ⇓⇓⇓⇓⇓
